5f7b081c38ff448efe96b3b2be02a81cdf1a55d0
Laëtitia Le Men <lemen.laetitia@gmail.com>
2022-09-01 22:16:34 +0200

    update

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
